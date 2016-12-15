dcda256e21994fc44182e2cda6b9a0b6ec6d4c71
anasauce <anasauceda1843@gmail.com>
2016-12-14 20:22:54 -0800

    created DetailsPageMain component

--- src/components/DetailsPageMain.js
+++ src/components/DetailsPageMain.js
@@ -4,7 +4,17 @@ import '../styles/details_page_main.css'
 class DetailsPageMain extends Component {
   render() {
     return (
-      <div className="top-border"></div>
+      <div>
+        <div className="top-border">
+        </div>
+        <div className="page-main">
+          <div className="header">
+            <h1 className="page-main__title">Account details</h1>
+            <h2 className="page-main__subtitle">Manage your personal details</h2>
+          </div>
+          
+        </div>
+      </div>
     )
   }
 }
